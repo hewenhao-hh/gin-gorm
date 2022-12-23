2a4b59b0ee69b7d19f6e856fd216061b23030219
hewenhao <195700557@qq.com>
2022-12-23 18:13:59 +0800

    初始化路由

--- main.go
+++ main.go
@@ -1,36 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"gin-gorm/bootstrap"
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"strings"
 )
 
 func main() {
-	// 初始化 Gin 实例
-	r := gin.New()
-	r.Use(gin.Logger(), gin.Recovery())
-	// 注册一个路由
-	r.GET("/", func(c *gin.Context) {
-		// 以 JSON 格式响应
-		c.JSON(http.StatusOK, gin.H{
-			"Hello": "world!",
-		})
-	})
+	// new 一个 Gin Engine 实例
+	router := gin.New()
 
-	r.NoRoute(func(c *gin.Context) {
-		acceptString := c.Request.Header.Get("Accept")
-		if strings.Contains(acceptString, "text/html") {
-			// 如果是 HTML 的话
-			c.String(http.StatusNotFound, "页面返回 404")
-		} else {
-			// 默认返回 JSOn
-			c.JSON(http.StatusNotFound, gin.H{
-				"error_code":    404,
-				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
-			})
-		}
-	})
+	// 初始化路由绑定
+	bootstrap.SetupRoute(router)
+
 	// 运行服务
-	r.Run(":8000")
+	err := router.Run(":3000")
+	if err != nil {
+		// 错误处理，端口被占用了或者其他错误
+		fmt.Println(err.Error())
+	}
 }
