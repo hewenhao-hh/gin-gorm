bbc3d26c426eb5175d220d14a99dc00f6cb7c07a
hewenhao <195700557@qq.com>
2022-12-28 10:55:17 +0800

    JSON 解析和验证器封装

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -2,7 +2,6 @@
 package auth
 
 import (
-	"fmt"
 	v1 "gin-gorm/app/http/controllers/api/v1"
 	"gin-gorm/app/models/user"
 	"gin-gorm/app/requests"
@@ -21,27 +20,7 @@ func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 
 	// 初始化请求对象
 	request := requests.SignupPhoneExistRequest{}
-
-	// 解析 JSON 请求
-	if err := c.ShouldBindJSON(&request); err != nil {
-		// 解析失败，返回 422 状态码和错误信息
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"error": err.Error(),
-		})
-		// 打印错误信息
-		fmt.Println(err.Error())
-		// 出错了，中断请求
-		return
-	}
-
-	// 表单验证
-	errs := requests.ValidateSignupPhoneExist(&request, c)
-	// errs 返回长度等于零即通过，大于 0 即有错误发生
-	if len(errs) > 0 {
-		// 验证失败，返回 422 状态码和错误信息
-		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
-			"errors": errs,
-		})
+	if ok := requests.Validate(c, &request, requests.SignupPhoneExist); !ok {
 		return
 	}
 
--- app/requests/signup_request.go
+++ app/requests/signup_request.go
@@ -10,13 +10,11 @@ type SignupPhoneExistRequest struct {
 	Phone string `json:"phone,omitempty" valid:"phone"`
 }
 
-func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]string {
-
+func SignupPhoneExist(data interface{}, c *gin.Context) map[string][]string {
 	// 自定义验证规则
 	rules := govalidator.MapData{
 		"phone": []string{"required", "digits:11"},
 	}
-
 	// 自定义验证出错时的提示
 	messages := govalidator.MapData{
 		"phone": []string{
@@ -24,15 +22,5 @@ func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]str
 			"digits:手机号长度必须为 11 位的数字",
 		},
 	}
-
-	// 配置初始化
-	opts := govalidator.Options{
-		Data:          data,
-		Rules:         rules,
-		TagIdentifier: "valid", // 模型中的 Struct 标签标识符
-		Messages:      messages,
-	}
-
-	// 开始验证
-	return govalidator.New(opts).ValidateStruct()
+	return validate(data, rules, messages)
 }
