a7f296e49054108dbb03424baa04fe34dc103e37
hewenhao <195700557@qq.com>
2022-12-28 09:58:35 +0800

    手机是否注册接口

--- bootstrap/database.go
+++ bootstrap/database.go
@@ -3,6 +3,7 @@ package bootstrap
 import (
 	"errors"
 	"fmt"
+	"gin-gorm/app/models/user"
 	"gin-gorm/pkg/config"
 	"gin-gorm/pkg/database"
 	"gorm.io/driver/mysql"
@@ -47,4 +48,6 @@ func SetupDB() {
 	database.SQLDB.SetMaxIdleConns(config.GetInt("database.mysql.max_idle_connections"))
 	// 设置每个链接的过期时间
 	database.SQLDB.SetConnMaxLifetime(time.Duration(config.GetInt("database.mysql.max_life_seconds")) * time.Second)
+
+	database.DB.AutoMigrate(&user.User{})
 }
