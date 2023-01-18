78538ff879615df257ce4c7dc6bf4dc246a68ef7
何文豪 <195700557@qq.com>
2023-01-18 10:49:50 +0800

    手机 + 短信验证码登录

---
+++ app/http/controllers/api/v1/auth/login_controller.go
@@ -0,0 +1,43 @@
+package auth
+
+import (
+	"encoding/json"
+	"fmt"
+	v1 "gin-gorm/app/http/controllers/api/v1"
+	"gin-gorm/app/requests"
+	"gin-gorm/pkg/auth"
+	"gin-gorm/pkg/jwt"
+	"gin-gorm/pkg/response"
+	"github.com/gin-gonic/gin"
+)
+
+// LoginController 用户控制器
+type LoginController struct {
+	v1.BaseAPIController
+}
+
+// LoginByPhone 手机登录
+func (lc *LoginController) LoginByPhone(c *gin.Context) {
+	// 1. 验证表单
+	request := requests.LoginByPhoneRequest{}
+	if ok := requests.Validate(c, &request, requests.LoginByPhone); !ok {
+		return
+	}
+
+	// 2. 尝试登录
+	user, err := auth.LoginByPhone(request.Phone)
+
+	jsonBytes, err := json.Marshal(user)
+	fmt.Println(string(jsonBytes))
+	if err != nil {
+		// 失败，显示错误提示
+		response.Error(c, err, "账号不存在")
+	} else {
+		// 登录成功
+		token := jwt.NewJWT().IssueToken(user.GetStringID(), user.Name)
+
+		response.JSON(c, gin.H{
+			"token": token,
+		})
+	}
+}
