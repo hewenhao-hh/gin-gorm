257939514cb6affe4e257113cc984986138d60b1
何文豪 <195700557@qq.com>
2023-01-18 11:47:37 +0800

    auth 中间件

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -4,6 +4,8 @@ package auth
 import (
 	"errors"
 	"gin-gorm/app/models/user"
+	"gin-gorm/pkg/logger"
+	"github.com/gin-gonic/gin"
 )
 
 // Attempt 尝试登录
@@ -25,4 +27,20 @@ func LoginByPhone(phone string) (user.User, error) {
 		return user.User{}, errors.New("手机号未注册")
 	}
 	return userModel, nil
+}
+
+// CurrentUser 从 gin.context 中获取当前登录用户
+func CurrentUser(c *gin.Context) user.User {
+	userModel, ok := c.MustGet("current_user").(user.User)
+	if !ok {
+		logger.LogIf(errors.New("无法获取用户"))
+		return user.User{}
+	}
+	// db is now a *DB value
+	return userModel
+}
+
+// CurrentUID 从 gin.context 中获取当前登录用户 ID
+func CurrentUID(c *gin.Context) string {
+	return c.GetString("current_user_id")
 }
