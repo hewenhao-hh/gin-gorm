ede242019bba2586c8375a3b96298a775070f970
何文豪 <195700557@qq.com>
2023-01-29 10:14:19 +0800

    数据填充

---
+++ database/seeders/users_seeder.go
@@ -0,0 +1,32 @@
+package seeders
+
+import (
+	"fmt"
+	"gin-gorm/database/factories"
+	"gin-gorm/pkg/console"
+	"gin-gorm/pkg/logger"
+	"gin-gorm/pkg/seed"
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	// 添加 Seeder
+	seed.Add("SeedUsersTable", func(db *gorm.DB) {
+
+		// 创建 10 个用户对象
+		users := factories.MakeUsers(10)
+
+		// 批量创建用户（注意批量创建不会调用模型钩子）
+		result := db.Table("users").Create(&users)
+
+		// 记录错误
+		if err := result.Error; err != nil {
+			logger.LogIf(err)
+			return
+		}
+
+		// 打印运行情况
+		console.Success(fmt.Sprintf("Table [%v] %v rows seeded", result.Statement.Table, result.RowsAffected))
+	})
+}
