ad936e55390bcfb7bf1346465df94fd3352bb2e7
何文豪 <195700557@qq.com>
2023-01-29 21:41:11 +0800

    创建话题

---
+++ app/http/controllers/api/v1/topics_controller.go
@@ -0,0 +1,89 @@
+package v1
+
+import (
+	"gin-gorm/app/models/topic"
+	"gin-gorm/app/requests"
+	"gin-gorm/pkg/auth"
+	"gin-gorm/pkg/response"
+
+	"github.com/gin-gonic/gin"
+)
+
+type TopicsController struct {
+	BaseAPIController
+}
+
+func (ctrl *TopicsController) Index(c *gin.Context) {
+	topics := topic.All()
+	response.Data(c, topics)
+}
+
+func (ctrl *TopicsController) Show(c *gin.Context) {
+	topicModel := topic.Get(c.Param("id"))
+	if topicModel.ID == 0 {
+		response.Abort404(c)
+		return
+	}
+	response.Data(c, topicModel)
+}
+
+func (ctrl *TopicsController) Store(c *gin.Context) {
+
+	request := requests.TopicRequest{}
+	if ok := requests.Validate(c, &request, requests.TopicSave); !ok {
+		return
+	}
+
+	topicModel := topic.Topic{
+		Title:      request.Title,
+		Body:       request.Body,
+		CategoryID: request.CategoryID,
+		UserID:     auth.CurrentUID(c),
+	}
+	topicModel.Create()
+	if topicModel.ID > 0 {
+		response.Created(c, topicModel)
+	} else {
+		response.Abort500(c, "创建失败，请稍后尝试~")
+	}
+}
+
+func (ctrl *TopicsController) Update(c *gin.Context) {
+
+	topicModel := topic.Get(c.Param("id"))
+	if topicModel.ID == 0 {
+		response.Abort404(c)
+		return
+	}
+
+	request := requests.TopicRequest{}
+	bindOk := requests.Validate(c, &request, requests.TopicSave)
+	if !bindOk {
+		return
+	}
+
+	topicModel.Body = request.Body
+	rowsAffected := topicModel.Save()
+	if rowsAffected > 0 {
+		response.Data(c, topicModel)
+	} else {
+		response.Abort500(c, "更新失败，请稍后尝试~")
+	}
+}
+
+func (ctrl *TopicsController) Delete(c *gin.Context) {
+
+	topicModel := topic.Get(c.Param("id"))
+	if topicModel.ID == 0 {
+		response.Abort404(c)
+		return
+	}
+
+	rowsAffected := topicModel.Delete()
+	if rowsAffected > 0 {
+		response.Success(c)
+		return
+	}
+
+	response.Abort500(c, "删除失败，请稍后尝试~")
+}
--- routes/api.go
+++ routes/api.go
@@ -63,4 +63,11 @@ func RegisterAPIRoutes(r *gin.Engine) {
 		cgcGroup.PUT("/:id", middlewares.AuthJWT(), cgc.Update)
 		cgcGroup.DELETE("/:id", middlewares.AuthJWT(), cgc.Delete)
 	}
+
+	// 帖子
+	tpc := new(controllers.TopicsController)
+	tpcGroup := v1.Group("/topics")
+	{
+		tpcGroup.POST("", middlewares.AuthJWT(), tpc.Store)
+	}
 }
