3d9a5c89df564e6df96ae7d07888755dac4c5704
何文豪 <195700557@qq.com>
2023-01-29 22:51:16 +0800

    显示话题

--- app/models/topic/topic_util.go
+++ app/models/topic/topic_util.go
@@ -5,10 +5,11 @@ import (
 	"gin-gorm/pkg/database"
 	"gin-gorm/pkg/paginator"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm/clause"
 )
 
 func Get(idstr string) (topic Topic) {
-	database.DB.Where("id", idstr).First(&topic)
+	database.DB.Preload(clause.Associations).Where("id", idstr).First(&topic)
 	return
 }
 
